9cc7eb30e63813294862f5258eac74bf3ae9a3a3
dominiklaubach <d.laubach@panacoda.com>
2011-11-26 00:09:06 +0100

    CouchCup's initial commit

---
+++ CouchCup/app/main.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 
+//
+// Project: CouchCup 
+// ==========================================================================
+
+var CouchCup  = CouchCup || {};
+
+CouchCup.app = M.Application.design({
+
+    /* Define the entry/start page of your app. This property must be provided! */
+    entryPage : 'page1',
+
+    page1: M.PageView.design({
+
+        //Add your code here!
+
+    })
+
+});
